2f8cbe13b6a3adcb69dfefa633a8c04698b1dd97
Andrey de Abreu <54045198+dreymond1@users.noreply.github.com>
2024-11-19 23:11:30 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sentiment_analize import prever_sentimento  # Importe o arquivo limpo como um módulo
 
 st.set_page_config(page_title="Meu Site Streamlit")
 
@@ -11,22 +12,6 @@ with st.container():
 
 
 @st.cache_data
-def carregar_dados():
-    tabela = pd.read_csv("resultados.csv")
-    return tabela
-
-with st.container():
-    st.write("---")
-    qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
-    num_dias = int(qtde_dias.replace("D", ""))
-    dados = carregar_dados()
-    dados = dados[-num_dias:]
-    st.area_chart(dados, x="Data", y="Contratos")
-
-
-import streamlit as st
-from sentiment_analize import prever_sentimento  # Importe o arquivo limpo como um módulo
-
 # Título da Aplicação
 st.title("Análise de Sentimentos com Naive Bayes")
 
