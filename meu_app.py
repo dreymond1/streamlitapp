5c2034a1173828eff8975c80ddba42ca8f7c3c5b
Andrey de Abreu <54045198+dreymond1@users.noreply.github.com>
2024-11-19 23:22:24 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sentiment_analysis import prever_sentimento  # Importe o arquivo limpo como um módulo
 
 st.set_page_config(page_title="Meu Site Streamlit")
 
@@ -22,3 +23,17 @@ with st.container():
     dados = carregar_dados()
     dados = dados[-num_dias:]
     st.area_chart(dados, x="Data", y="Contratos")
+
+# Título da Aplicação
+st.title("Análise de Sentimentos com Naive Bayes")
+
+# Entrada de Texto
+text_input = st.text_area("Digite um comentário para análise:")
+
+# Botão de Previsão
+if st.button("Analisar Sentimento"):
+    if text_input.strip():
+        sentimento = prever_sentimento(text_input)
+        st.success(f"Sentimento previsto: {sentimento}")
+    else:
+        st.warning("Por favor, insira um texto.")
